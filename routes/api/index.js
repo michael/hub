c45e34cc187faf1df8cd7876b4e6c140166c8452
Michael Aufreiter <michael@substance.io>
2013-01-22 20:52:15 +0100

    Sketch Replication API.

--- routes/api/index.js
+++ routes/api/index.js
@@ -94,4 +94,48 @@ apis.configure = function (app) {
       res.json({"status": "ok", "token": db.uuid(), "username": params.username});
     });
   });
+
+
+  // Replication API
+  // ===================
+  // 
+  // TODO: Move to separate file
+
+
+  // Query document states by username
+  // -----------
+
+  app.post('/document_states/:user', function(req, res, next) {
+    var user = req.params.user;
+    store.getDocStates(function(err, documents) {
+      res.json(documents);
+    });
+  });
+
+
+  // Pull Commits
+  // -----------
+
+  app.post('/pull_commits/:document/:synced_commit', function(req, res, next) {
+    var synced_commit = req.params.synced_commit;
+
+    // Returns all commits after synced_commit
+    store.getCommits(synced_commit, function(err, commits) {
+      res.json(commits);
+    });
+
+  });
+
+  // Push Commits
+  // -----------
+
+  app.post('/push_commits', function(req, res, next) {
+    var commits = JSON.parse(req.body.data);
+    var document = req.body.document;
+    var synced_commit = req.body.synced_commit;
+    store.update(document, commits, function(err) {
+      res.json({"status": "ok"});
+    });
+  });
+
 };
