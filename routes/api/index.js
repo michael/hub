73d37b2c99cfa20771edd610c18934d20c57987b
Pier Paolo Ramon <ramonpierre@gmail.com>
2013-01-26 17:27:25 +0100

    Good step froward complete authentication

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,12 +3,67 @@ var passport = require('passport');
 
 var dir = '../../lib/';
 
+var errors = require(dir + 'errors');
 var csrf = require(dir + 'csrf');
 var users = require(dir + 'users');
 var publications = require(dir + 'publications');
+var authorizations = require(dir + 'authorizations');
 
 var apis = module.exports = {};
 
+function authenticate (types, options, callback) {
+  if (typeof options === 'function') {
+    callback = options;
+    options = {};
+  }
+  return function (req, res, next) {
+    passport.authenticate(types, options, function (err, user, info) {
+      var args = Array.prototype.slice.call(arguments);
+      callback.apply(this, [req, res, next].concat(args));
+    }).apply(passport, arguments);
+  };
+}
+
+function authenticateCommon () {
+  return passport.authenticate(['hash', 'basic'], {
+    session: false
+  });
+}
+
+function authenticateUser () {
+  return passport.authenticate('basic', {
+    session: false
+  });
+}
+
+function authenticateApplication () {
+  return authenticate('oauth2-client-password', function (req, res, next, err, client) {
+    if (err) {
+      next(err);
+    } else if (!client) {
+      res.status(401).end('Unauthorized');
+    } else {
+      req.client = client;
+      next();
+    }
+  });
+}
+
+function out (res) {
+  return function (err, data) {
+
+    var status = 200;
+    if (err instanceof errors.NoRecordFound) status = 404;
+    else if (err) status = 500;
+
+    if (err) res.json(status, {
+      type: err.name,
+      message: err.message
+    });
+    else if (data) res.json(status, data);
+    else res.json(status, {});
+  };
+}
 
 apis.configure = function (app) {
 
@@ -16,14 +71,36 @@ apis.configure = function (app) {
     res.json({});
   });
 
-  app.post('/token',
-    passport.authenticate('oauth2-client-password', {
+  app.post('/authorization',
+    authenticateUser(),
+    authenticateApplication(),
+    function (req, res) {
+      var user = req.user;
+      var application = req.client;
+      authorizations.findOrCreate(user.uuid, application.uuid, 'all', out(res));
+    });
+
+  app.get('/authorization',
+    authenticateUser(),
+    authenticateApplication(),
+    function (req, res) {
+      var user = req.user;
+      var application = req.client;
+      authorizations.findByUserAndApplication(user.uuid, application.uuid, out(res));
+    });
+
+  app.get('/authorizations',
+    authenticateCommon(),
+    function (req, res) {
+      authorizations.findByUser(req.user.uuid, out(res));
+    });
+
+  app.get('/authorizations/:uuid',
+    passport.authenticate(['local', 'basic', 'hash'], {
       session: false
     }),
-    app.oauth.token(),
-    app.oauth.errorHandler(),
     function (req, res) {
-      res.json({ ok: '?' });
+      authorizations.secureFindById(req.user.uuid, req.params.uuid, out(res));
     });
 
 
