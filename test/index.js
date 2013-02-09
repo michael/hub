f705b47d0901721fb8fec72c6a726fc8e0fd0557
Pier Paolo Ramon <ramonpierre@gmail.com>
2013-02-09 18:20:00 +0100

    New bunch of tests. Moved to double quotes.

--- test/index.js
+++ test/index.js
@@ -1,23 +1,25 @@
+/*jshint expr:true */
 
-var request = require('supertest');
-var should = require('should');
 
-var app = require('../app')({
+var request = require("supertest");
+var should = require("should");
+
+var app = require("../app")({
   logger: { transports: [] }
 });
 
 request = request(app);
 
-var GET = 'get';
-var POST = 'post';
-var PUT = 'put';
-var DELETE = 'delete';
+var GET = "get";
+var POST = "post";
+var PUT = "put";
+var DELETE = "del";
 
 function api (verb, path, obj) {
 
-  var req = request[verb]('/api/v1'+(path||''))
-    .set('Accept', 'application/json')
-    .expect('Content-Type', /json/);
+  var req = request[verb]("/api/v1"+(path||""))
+    .set("Accept", "application/json")
+    .expect("Content-Type", /json/);
 
   if (obj && obj.secret) {
     req = req.send({
@@ -25,27 +27,27 @@ function api (verb, path, obj) {
       client_secret: obj.secret
     });
   } else if (obj && obj.token) {
-    req = req.set('Authorization', 'token '+obj.token);
+    req = req.set("Authorization", "token "+obj.token);
   }
 
   return req;
 }
 
 
-describe('Simple smoke tests', function () {
+describe("Simple smoke tests", function () {
 
-  it('responds with the home page', function (done) {
-    request.get('/')
+  it("responds with the home page", function (done) {
+    request.get("/")
       .expect(200, done);
   });
 
 });
 
 
-describe('APIs', function () {
+describe("APIs", function () {
 
-  it('should give the links', function (done) {
-    api(GET, '')
+  it("should give the links", function (done) {
+    api(GET, "")
       .expect(200, done);
   });
 
@@ -56,45 +58,52 @@ var Composer;
 var Auth;
 
 
-describe('Authorization APIs', function () {
+describe("Authorization APIs", function () {
 
-  it('should throw without authentication', function (done) {
-    api(POST, '/authorizations')
+  it("should throw without authentication", function (done) {
+    api(POST, "/authorizations")
       .expect(/authentication|application/i)
       .expect(401, done);
   });
 
-  it('should throw without application credentials', function (done) {
-    api(POST, '/authorizations')
-      .auth('admin', 'unicornsarecute')
+  it("should throw without application credentials", function (done) {
+    api(POST, "/authorizations")
+      .auth("admin", "unicornsarecute")
       .expect(/application/i)
       .expect(401, done);
   });
 
-  it('should throw without user credentials', function (done) {
-    app.applications.findByName('Composer', function (err, application) {
+  it("should throw without user credentials", function (done) {
+    app.applications.findByName("Composer", function (err, application) {
       Composer = application;
-      if (err) done(err);
-      else api(POST, '/authorizations', Composer)
+      if (err) return done(err);
+      api(POST, "/authorizations", Composer)
         .expect(/authentication/i)
         .expect(401, done);
     });
   });
 
-  it('should return an authorization', function (done) {
-    api(POST, '/authorizations', Composer)
-      .auth('admin', 'unicornsarecute')
+  it("should return an authorization", function (done) {
+    api(POST, "/authorizations", Composer)
+      .auth("admin", "unicornsarecute")
       .expect(200)
       .end(function (err, res) {
         if (err) return done(err);
         Auth = res.body;
-        Auth.should.have.property('token');
         done();
       });
   });
 
-  it('should be accessible with the provided token', function (done) {
-    api(GET, '/authorizations', Auth)
+  it("should return a valid authorization", function () {
+    Auth.should.have.property("token");
+    Auth.should.have.property("uuid");
+    Auth.should.have.property("application_uuid");
+    Auth.token.should.be.ok;
+    Auth.application_uuid.should.equal(Composer.uuid);
+  });
+
+  it("should be accessible with the provided token", function (done) {
+    api(GET, "/authorizations", Auth)
       .expect(200)
       .end(function (err, res) {
         if (err) return done(err);
@@ -102,6 +111,27 @@ describe('Authorization APIs', function () {
         res.body.should.includeEql(Auth);
         done();
       });
+  });
+
+  it("should return the provided token as current", function (done) {
+    api(GET, "/authorizations/current", Composer)
+      .auth("admin", "unicornsarecute")
+      .expect(200)
+      .end(function (err, res) {
+        if (err) return done(err);
+        res.body.should.eql(Auth);
+        done();
+      });
+  });
+
+  it("shouldn't permit updates", function (done) {
+    api(PUT, "/authorizations/"+Auth.uuid)
+      .expect(501, done);
+  });
+
+  it("shouldn't permit deletions", function (done) {
+    api(DELETE, "/authorizations/"+Auth.uuid)
+      .expect(501, done);
   });
 
 });
