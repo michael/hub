1506156fa162aed15bf2e43c2f2b4f60f6c5ed62
Michael Aufreiter <michael@substance.io>
2013-05-13 10:54:54 +0200

    Hub updates.

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ module.exports = function create(options) {
     app.use(routes.commonHelper);
     app.use(gravatars);
     app.use(express['static'](path.join(__dirname, 'assets')));
-    app.use(express['static'](path.join(__dirname, 'assets', 'composer')));
+    // app.use(express['static'](path.join(__dirname, 'assets', 'composer')));
 
     app.use(assets());
     app.use(app.router);
